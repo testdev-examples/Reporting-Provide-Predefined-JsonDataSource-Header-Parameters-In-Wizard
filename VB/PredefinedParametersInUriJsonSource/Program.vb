9074b25db037c16cecbd9e43e1efbf4accb99c2d
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2019-11-04 17:16:22 +0300

    auto sync for version 2019.1

--- VB/PredefinedParametersInUriJsonSource/Program.vb
+++ VB/PredefinedParametersInUriJsonSource/Program.vb
@@ -5,12 +5,16 @@ Imports System.Threading.Tasks
 Imports System.Windows.Forms
 
 Namespace PredefinedParametersInUriJsonSource
-	Friend Module Program
-		<STAThread>
-		Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Form1())
-		End Sub
-	End Module
+    Friend NotInheritable Class Program
+
+        Private Sub New()
+        End Sub
+
+        <STAThread> _
+        Shared Sub Main()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Application.Run(New Form1())
+        End Sub
+    End Class
 End Namespace

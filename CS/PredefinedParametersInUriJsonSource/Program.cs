fb5410a1db4d801535da7b3c874138166778c85e
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2020-04-21 05:02:46 +0300

    auto sync for version 2020.1

--- CS/PredefinedParametersInUriJsonSource/Program.cs
+++ CS/PredefinedParametersInUriJsonSource/Program.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading.Tasks;
 
 namespace PredefinedParametersInUriJsonSource {
     static class Program {
--- VB/PredefinedParametersInUriJsonSource/Program.vb
+++ VB/PredefinedParametersInUriJsonSource/Program.vb
@@ -1,20 +1,16 @@
 ﻿Imports System
 Imports System.Collections.Generic
 Imports System.Linq
-Imports System.Threading.Tasks
 Imports System.Windows.Forms
+Imports System.Threading.Tasks
 
 Namespace PredefinedParametersInUriJsonSource
-    Friend NotInheritable Class Program
-
-        Private Sub New()
-        End Sub
-
-        <STAThread> _
-        Shared Sub Main()
-            Application.EnableVisualStyles()
-            Application.SetCompatibleTextRenderingDefault(False)
-            Application.Run(New Form1())
-        End Sub
-    End Class
+	Friend Module Program
+		<STAThread>
+		Sub Main()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+		End Sub
+	End Module
 End Namespace
